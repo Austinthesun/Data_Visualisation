074df5e049a16ed3d6d44349ccafb73303ef51a7
Aniemeka Onono <aniemekaonono1@gmail.com>
2021-04-14 10:47:48 +1000

    Added functions to dynamically plot the range of argument.

--- cubes.py
+++ cubes.py
@@ -1,16 +1,21 @@
 import matplotlib.pyplot as plt
 
-x_values = [1, 2, 3, 4, 5]
-y_values = [x**3 for x in x_values]
+def cubes_of_numbers(numbers):
 
-plt.style.use('seaborn')
-fig, ax = plt.subplots()
-ax.plot(x_values, y_values, c='red', linewidth=3)
+    x_values = range(1, numbers + 1)
+    y_values = [x**3 for x in x_values]
 
-ax.set_title("Cubes", fontsize=24)
-ax.set_xlabel("Value", fontsize=14)
-ax.set_ylabel("Cubes of Value", fontsize=14)
+    plt.style.use('seaborn')
+    fig, ax = plt.subplots()
+    ax.plot(x_values, y_values, c='red', linewidth=3)
 
-ax.tick_params(axis='both', which='major', labelsize=14)
+    ax.set_title("Cubes", fontsize=24)
+    ax.set_xlabel("Value", fontsize=14)
+    ax.set_ylabel("Cubes of Value", fontsize=14)
 
-plt.show()
+
+    ax.tick_params(axis='both', which='major', labelsize=14)
+    plt.show()
+
+#cubes_of_numbers(5)
+cubes_of_numbers(5000)
